0364436f7ea7915b39fc553b0e4f072de2375c9e
Rodgersouko <gabrielaouko@gmail.com>
2020-12-02 05:22:24 +0300

    create registration API

--- myhood/urls.py
+++ myhood/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .api import RegistrationAPI
+from knox import views as knox_views
 
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('', views.home, name='home'),
+    path('register/', RegistrationAPI.as_view()),
 ]
