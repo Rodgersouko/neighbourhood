9a884b52ebda87d43d22305cc93c471938acab8c
Rodgersouko <gabrielaouko@gmail.com>
2020-12-02 05:39:51 +0300

    create login API & User API to show data

--- myhood/serializers.py
+++ myhood/serializers.py
@@ -18,3 +18,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username')
+
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid Details.")
--- myhood/urls.py
+++ myhood/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .api import RegistrationAPI
 from knox import views as knox_views
-
+from .api import RegistrationAPI, LoginAPI, UserAPI
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', RegistrationAPI.as_view()),
+    path('login/', LoginAPI.as_view()),
+    path('user/', UserAPI.as_view()),
 ]
