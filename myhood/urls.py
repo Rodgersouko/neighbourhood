a6c3bcd0d464d1e5feb2649975bd5640a0262412
Rodgersouko <gabrielaouko@gmail.com>
2020-12-02 06:12:58 +0300

    add path in urls for logout funconality

--- myhood/urls.py
+++ myhood/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', RegistrationAPI.as_view()),
     path('login/', LoginAPI.as_view()),
     path('user/', UserAPI.as_view()),
+    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
