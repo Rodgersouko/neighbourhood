2c03f8243fe9bf0243df743bc72df4ab584ce1e4
REAGAN2020 <reaganteflon@gmail.com>
2020-12-05 23:00:11 +0300

    added some changes in admin

--- myhood/admin.py
+++ myhood/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Profile,Business,Post,Hood
-# Register your models here.
-admin.site.register(Hood)
+from .models import *
+
+admin.site.register(Neighbourhood)
 admin.site.register(Profile)
 admin.site.register(Business)
 admin.site.register(Post)
