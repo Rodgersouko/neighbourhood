14dc424e55cd291f7c74fb8bf4676fc55c98a508
REAGAN2020 <reaganteflon@gmail.com>
2020-12-05 22:49:45 +0300

    added serializer

--- myhood/serializers.py
+++ myhood/serializers.py
@@ -1,50 +1,56 @@
-from rest_framework import generics, permissions, serializers, exceptions
-from django.contrib.auth.models import User
-from .models import  Hood,Profile,Business,Post
+from rest_framework import serializers
+from .models import *
+from django.contrib.auth.hashers import make_password
+from rest_framework import viewsets
 
-# User Serializer
+
+
+
 class UserSerializer(serializers.ModelSerializer):
+    # neighbourhood = serializers.CharField(source='neighbourhood.name')
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
-
-# Register Serializer
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
+        fields = ['first_name', 'email', 'is_staff', 'last_name', 'avatar']
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
-
-        return user
-
-class ChangePasswordSerializer(serializers.Serializer):
-    model = User
-
-    """
-    Serializer for password change endpoint.
-    """
-    old_password = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True)
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
+class UserRegistrationSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    password = serializers.CharField()
+    confirm_password = serializers.CharField()
+    def validate_email(self, email):
+        existing = User.objects.filter(email=email).first()
+        if existing:
+            raise serializers.ValidationError("Someone with that email "
+                "address has already registered. Was it you?")
+        return email
+    def validate(self, data):
+        if not data.get('password') or not data.get('confirm_password'):
+            raise serializers.ValidationError("Please enter a password and "
+                "confirm it.")
+        if data.get('password') != data.get('confirm_password'):
+            raise serializers.ValidationError("Those passwords don't match.")
+        return data
+        
+class NeighbourhoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Neighbourhood
+        fields = ('id', 'name', 'location','admin', 'occupantsCount','image')
+        
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['id', 'name', 'profile_pic','estate','contact']
-
-class HoodSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hood
-        fields = ['id', 'Name', 'Count','location','Foreignkey']
-
+        fields = ('id', 'name', 'email', 'status', 'image','user')
+        
 class BusinessSerializer(serializers.ModelSerializer):
     class Meta:
         model = Business
-        fields = ['id', 'name', 'description','owner','location']
-
+        fields = ('id', 'business_name','user','neighbourhood', 'business_email','business_profile')
+        
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content','owner','hood']
+        fields = ('id', 'title', 'user','neighbourhood','text','image')
