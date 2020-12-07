b345542711bdae8b2835209c9ea6a87182caaa62
REAGAN2020 <reaganteflon@gmail.com>
2020-12-07 16:01:55 +0300

    modified

--- myhood/serializers.py
+++ myhood/serializers.py
@@ -27,7 +27,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            validated_data['first_name'], validated_data['email'], validated_data['password'])
+           validated_data['email'], validated_data['password'])
         return user
 
 
